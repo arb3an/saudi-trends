e5d427972c8a197c7a820d67e3d9317245aa3692
arb3an <a.rb3an@gmail.com>
2025-11-12 16:25:22 +0300

    Remove outdated comment in vite.config.ts
    Remove comment about building from client to dist/client

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// نبني من مجلد client ونخرج إلى dist/client
 export default defineConfig({
   root: "client",
   plugins: [react()],
